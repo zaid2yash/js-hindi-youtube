ca2a567ee35d2ceaf110bdcd6503ba04ea0da274
Mohd Zaid <88224255+zaid2yash@users.noreply.github.com>
2024-02-11 07:55:42 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert ( 3 + 3 )// we are using nodejs, not browser
+
+console.log(3
+    + 
+     3) // code readability should be high
+
+     console.log("Zaid")
+
+     let name = "Zaid"
+     let age = 24
+     let isLoggedIn = false
+     let state = null
+
+     // number => 2 to power 53
+     // bigint =>
+     // string => ""
+     // boolean => true/false 
+     // null => standalone value
+     // undefined => undefined
+     // symbol => unique
+
+
+     // object
+
+     console.log(typeof null); //object
+     console.log(typeof undefined);// undefined
